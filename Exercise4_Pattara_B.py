3a3244905741c20c2e677b42b8ad5bd6b2d59593
pattarabh <76655571+pattarabh@users.noreply.github.com>
2021-01-02 11:56:59 +0700

    Submitted Ex4

---
+++ Exercise4_Pattara_B.py
@@ -0,0 +1,9 @@
+FE = 85.00 #Foundation English
+GE = 72.00 #General Business
+ICS = 81.95 #Introduction to Computer Systems
+CP = 90.05 #Computer Programming
+print("---Your Score---")
+print("Foundation English               : ",FE)
+print("General Business                 : ",GE)
+print("Introduction to Computer Systems : ",ICS)
+print("Computer Programming             : ",CP)
